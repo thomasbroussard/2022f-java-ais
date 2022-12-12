0efc2bd4ebd3477d84ab810315c4f0ee1bbffd02
Thomas <thomas.broussard@gmail.com>
2022-12-12 10:09:11 +0100

    * introduced test case approach with files

--- project/src/test/java/fr/epita/test/TestFiles.java
+++ project/src/test/java/fr/epita/test/TestFiles.java
@@ -1,8 +1,12 @@
 package fr.epita.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 
 public class TestFiles {
 
@@ -21,16 +25,29 @@ public class TestFiles {
     }
 
     @Test
-    public void testFile(){
+    public void testFile() throws IOException {
+
+        //given
         File file = new File("test.save");
-        if (file.exists()){
-            System.out.println("exists");
-        } else {
-            System.out.println("does not exist");
+        try {
+            if (!Files.exists(file.toPath())) {
+                Files.createFile(file.toPath());
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
 
+        //then
+//        if (file.exists()){
+//            System.out.println("exists");
+//        } else {
+//            System.out.println("does not exist");
+//        }
 
+        Assertions.assertTrue(file.exists());
 
+        //clean
+        Files.delete(file.toPath());
     }
 
 
