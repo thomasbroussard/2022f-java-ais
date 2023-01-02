cfc28a15a62779bd5b0bfa63852976b1277c1a29
Thomas <thomas.broussard@gmail.com>
2023-01-02 16:32:25 +0100

    * implemented unit test case on read function

--- project/src/test/java/fr/epita/test/CSVServiceTest.java
+++ project/src/test/java/fr/epita/test/CSVServiceTest.java
@@ -1,5 +1,8 @@
 package fr.epita.test;
 
+import fr.epita.bank.datamodel.Account;
+import fr.epita.bank.datamodel.InvestmentAccount;
+import fr.epita.bank.datamodel.Stock;
 import fr.epita.bank.datamodel.StockOrder;
 import fr.epita.bank.exceptions.CSVServiceInitializationException;
 import fr.epita.bank.services.CSVService;
@@ -7,6 +10,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,8 +19,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class CSVServiceTest {
 
     @Test
-    void write() {
-        //TODO write a test to validate write function works correctly
+    void write() throws CSVServiceInitializationException, IOException {
+        //given
+        File outputFile = new File("unit-test-write-CSVServiceTest.csv");
+        CSVService service  = new CSVService(outputFile);
+        Stock stock = new Stock("silver");
+        Account coreAccount = new Account(0);
+        InvestmentAccount account = new InvestmentAccount(coreAccount);
+        coreAccount.setId(0);
+
+        StockOrder stockOrder = new StockOrder(stock,
+                account,
+                "2023-01-02",
+                20.0,
+                2,
+                2.0);
+
+        //when
+        service.write(stockOrder);
+
+        //then
+        List<String> lines = Files.readAllLines(outputFile.toPath());
+        String s = lines.get(1);
+        Assertions.assertTrue(s.startsWith("silver"));
+
     }
 
     @Test
