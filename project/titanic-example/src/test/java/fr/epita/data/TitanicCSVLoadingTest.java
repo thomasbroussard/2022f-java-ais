68bc3b1aaf881398eb31ac0702bab86929d718b1
Thomas <thomas.broussard@gmail.com>
2023-01-09 16:10:54 +0100

    * implemented group by using a map

--- project/titanic-example/src/test/java/fr/epita/data/TitanicCSVLoadingTest.java
+++ project/titanic-example/src/test/java/fr/epita/data/TitanicCSVLoadingTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TitanicCSVLoadingTest {
 
@@ -37,17 +38,51 @@ public class TitanicCSVLoadingTest {
         List<Passenger> completePassengersList = passengerCSVService.readAll(); // create Passenger datamodel + readAll() method
 
         Map<Integer, Integer> groupByPclassCount = new LinkedHashMap<>();
-        for (Passenger passenger : completePassengersList){
+        for (Passenger passenger : completePassengersList) {
             Integer count = groupByPclassCount.get(passenger.getpClass());
-            if (count == null){
+            if (count == null) {
                 count = 1;
-            }else{
-                count ++;
+            } else {
+                count++;
             }
             groupByPclassCount.put(passenger.getpClass(), count);
         }
 
         System.out.println(groupByPclassCount);
 
+    }
+
+    @Test
+    public void testGroupByAge() throws IOException {
+        //given
+        File csvFile = new File("titanic-dataset/data.csv");// load the file
+        PassengerCSVService passengerCSVService = new PassengerCSVService(csvFile); // create PassengerCSVService
+        List<Passenger> completePassengersList = passengerCSVService.readAll(); // create Passenger datamodel + readAll() method
+
+        Map<Double, Integer> groupByAgeCount = new LinkedHashMap<>();
+        for (Passenger passenger : completePassengersList) {
+            Integer count = groupByAgeCount.getOrDefault(passenger.getAge(), 0) + 1;
+            groupByAgeCount.put(passenger.getAge(), count);
+        }
+
+        System.out.println(groupByAgeCount);
+    }
+
+    @Test
+    public void testGroupUsingStream() throws IOException {
+        //given
+        File csvFile = new File("titanic-dataset/data.csv");// load the file
+        PassengerCSVService passengerCSVService = new PassengerCSVService(csvFile); // create PassengerCSVService
+        List<Passenger> completePassengersList = passengerCSVService.readAll(); // create Passenger datamodel + readAll() method
+
+        Double averageAge = completePassengersList
+                .stream()
+                .mapToDouble(passenger -> passenger.getAge())
+                .average()
+                .getAsDouble();
+
+        Map<Double, List<Passenger>> collect = completePassengersList
+                .stream()
+                .collect(Collectors.groupingBy(passenger -> passenger.getAge()));
     }
 }
