484c2bab16b7a63430de43f1a5f4b5fa315007f2
Thomas <thomas.broussard@gmail.com>
2023-01-09 15:56:30 +0100

    * implemented group by using a map

--- project/titanic-example/src/test/java/fr/epita/data/TitanicCSVLoadingTest.java
+++ project/titanic-example/src/test/java/fr/epita/data/TitanicCSVLoadingTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TitanicCSVLoadingTest {
 
@@ -25,6 +27,27 @@ public class TitanicCSVLoadingTest {
         //then
         System.out.println(completePassengersList.size());
         Assertions.assertTrue(completePassengersList.size() == 756);
+    }
+
+    @Test
+    public void testGroupBy() throws IOException {
+        //given
+        File csvFile = new File("titanic-dataset/data.csv");// load the file
+        PassengerCSVService passengerCSVService = new PassengerCSVService(csvFile); // create PassengerCSVService
+        List<Passenger> completePassengersList = passengerCSVService.readAll(); // create Passenger datamodel + readAll() method
+
+        Map<Integer, Integer> groupByPclassCount = new LinkedHashMap<>();
+        for (Passenger passenger : completePassengersList){
+            Integer count = groupByPclassCount.get(passenger.getpClass());
+            if (count == null){
+                count = 1;
+            }else{
+                count ++;
+            }
+            groupByPclassCount.put(passenger.getpClass(), count);
+        }
+
+        System.out.println(groupByPclassCount);
 
     }
 }
